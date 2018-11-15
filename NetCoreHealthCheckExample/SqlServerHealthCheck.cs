c97924f8cd5290994885a8fbc27520cc43ffd8b2
Kovtun Volodymyr <Kovtun1994@gmail.com>
2018-11-15 16:02:54 +0200

    init commit

---
+++ NetCoreHealthCheckExample/SqlServerHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetCoreHealthCheckExample
+{
+    public class SqlServerHealthCheck : IHealthCheck
+    {
+        //just for demonstrate.In real live this should be stored in some secure place
+        private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=test;Trusted_Connection=True;ConnectRetryCount=0";
+        public string Name => "sql";
+
+        public SqlServerHealthCheck()
+        {
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(cancellationToken);
+                }
+                catch (SqlException)
+                {
+                    return HealthCheckResult.Failed();
+                }
+            }
+
+            return HealthCheckResult.Passed();
+        }
+    }
+}
---
+++ NetCoreHealthCheckExample/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace NetCoreHealthCheckExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services
+                .AddHealthChecks()
+                .AddCheck<SqlServerHealthCheck>("sql");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseHealthChecks("/healthcheck", BuildHealthCheckOptions());
+        }
+
+        private HealthCheckOptions BuildHealthCheckOptions()
+        {
+            var options = new HealthCheckOptions();
+            options.ResponseWriter = async (c, r) =>
+            {
+                c.Response.ContentType = "application/json";
+
+                var result = JsonConvert.SerializeObject(new
+                {
+                    status = r.Status.ToString(),
+                    errors = r.Entries.Select(e => new { key = e.Key, value = e.Value.Status.ToString(), description = e.Value.Description })
+                });
+                await c.Response.WriteAsync(result);
+            };
+
+            return options;
+        }
+    }
+}
